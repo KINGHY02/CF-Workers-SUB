492dd6278a12f529e599567340eab062a5808020
KINGHY02 <3148213528@qq.com>
2026-05-11 09:53:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -240,7 +240,7 @@ async function nginx() {
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>KiNG博客入口</title>
+<title>KiNG Blog - 国王云</title>
 <style>
   body {
     margin:0;
